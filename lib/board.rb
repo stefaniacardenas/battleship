e00a98933df3faa248e3fe1f5cfd28dc9028052e
DLDB <dpbergquist@gmail.com>
2014-04-22 14:53:58 +0100

    added board initialize

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,17 @@
 class Board
 
-	def rows
+  def initialize(player='norman_no_name')
+    @player = player
+  end
+
+  def rows
 		board_array = []
 		10.times{board_array << (1..10).to_a}
 		return board_array
 	end
+
+  def owner
+    @player.name
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,13 @@
 require "board"
 
 describe Board do 
-	let(:board) {Board.new}
+  let(:player) {double :player, name: 'Bob'}
+	let(:board)  {Board.new(player)}
+
+  it "has a player when initialized." do
+    expect(board.owner).to eq 'Bob'
+  end  
+
 	it "returns one array with ten elements" do
 		expect(board.rows.count).to eq 10
 	end
@@ -9,5 +15,9 @@ describe Board do
 	it "each element of the array is an array" do 
 		expect(board.rows[0].class).to eq Array
 	end 
-	
+
+  it "each array within main array contains 10 elements" do
+    expect(board.rows[0].count).to eq 10
+  end
+
 end 
